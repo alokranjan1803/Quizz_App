a8c03c4d89f462bd41dfcd8703ae33c602b6fd63
alokfighter1803 <alok1832003@gmail.com>
2023-06-14 10:03:55 +0530

    This is my first project

---
+++ android/app/src/main/kotlin/com/example/quizzzz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizzzz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:quizzzz/quiz.dart';
+
+
+void main() {
+  runApp(const Quiz());
+
+}
